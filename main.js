745abf8982d43c7771659fc25c2cd3f7292df573
anubhavsaraswat <84270616+anubhavsaraswat@users.noreply.github.com>
2021-09-30 13:28:52 +0530

    mark13- Done

--- main.js
+++ main.js
@@ -65,10 +65,99 @@ function checkPalindrome(date){
     return flag;
 }
 
-var date={
-    day:2,
-    month:9,
-    year:2020
+function checkLeapYear(year){
+ if(year%400===0){
+     return true;
+ } 
+ else if(year%100===0){
+     return false;
+ }
+ else if(year%4===0){
+     return true;
+ }
+ else {
+     return false;
+ }
+
 }
 
-console.log(checkPalindrome(date));
+function giveNextDate(date){
+    var day =date.day+1;
+    var month=date.month;
+    var year=date.year;
+
+    var numberofDaysInMonth =[31,28,31,30,31,30,31,31,30,31,30,31]
+
+     // check for feb month
+if(month === 2){ 
+    // check for leap year
+    if(isLeapYear(year)){ 
+       if(day > 29){ 
+         day = 1;
+         month++;  // increment the month
+       }
+    }
+    else {
+       if(day > 28){
+         day = 1;
+         month++;  // increment the month
+       }
+    }
+  }
+  // check for other months
+  else {
+    //  check if the day exceeds the max days in month
+    if(day > numberofDaysInMonth[month - 1]){ 
+      day = 1; 
+      month++;  // increment the month
+    }
+  }
+
+  // increment the year if month is greater than 12
+  if(month > 12){
+    month = 1;
+    year++; 
+  }
+
+  return {
+    day: day,  
+    month: month,
+    year: year
+  };
+}
+// var date={
+//         day:2,
+//         month:9,
+//         year:2020
+//     }
+
+// console.log(giveNextDate(date))
+
+
+var dateInput = document.querySelector('#user-input');
+var checkBtn = document.querySelector('#button');
+var result = document.querySelector('#result');
+
+function clickHandler(){
+  var bdayStr = dateInput.value; 
+  if(bdayStr !== ''){
+    var listOfDate = bdayStr.split('-'); 
+
+    var date = {
+      day: Number(listOfDate[2]),
+      month: Number(listOfDate[1]),
+      year: Number(listOfDate[0])
+    };
+    
+    var isPalindrome = checkPalindrome(date);
+
+    if(isPalindrome){
+       result.innerText = 'Hurray! your birthday is a palindrome!';
+    }
+    else {
+       result.innerText = "Oops ! your birthday is not a palindrome!"
+    }
+  }
+}
+
+checkBtn.addEventListener('click', clickHandler);
